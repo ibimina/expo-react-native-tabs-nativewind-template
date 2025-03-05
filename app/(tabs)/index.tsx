68fcdd0aa9b0503eb580f86da669a5c2c11f109a
ibimina hart <ibimina.c.hart@gmail.com>
2025-03-05 23:05:06 +0100

    expo react native tabs template with native wind

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 bg-blue-500">
+      <Text className="px-6 py-8 text-white">hello</Text>
+    </View>
+  );
+}
